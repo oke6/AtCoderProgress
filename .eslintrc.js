ae7691af00433b10e91fcef44097db19423f14c4
Ozaki Ken <ozaki@liberrace.com>
2022-06-29 23:36:23 +0900

    otintin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
     'no-unused-vars': 'off',
     'no-use-before-define': 'off',
